f230acc2d1fe464049f777a35858252a0cbce503
Lyubomir Yonchev <lyubomir.yonchev@limechain.tech>
2022-07-29 09:09:49 +0300

    config management added, more servides added

--- internal/app/tokenised-infrastructure-rewarder/services/nft.go
+++ internal/app/tokenised-infrastructure-rewarder/services/nft.go
@@ -1,4 +1,12 @@
 package services
 
+import (
+	config "github.com/CudoVentures/tokenised-infrastructure-rewarder/internal/app/tokenised-infrastructure-rewarder/infrastructure/config.go"
+)
+
 // should fetch nfts
 // filter them and extract the hashRateIds from them to be used in the pool fetching
+
+func GetAllNFTsFromHasura() {
+	HasuraURL := config
+}
