d1af65b9528f08229871db1e4b972eb0eb9588a0
infra1515 <lyubomir.yonchev@limechain.tech>
2022-12-15 16:14:47 +0200

    code refactor

--- internal/app/tokenised-infrastructure-rewarder/services/calculator.go
+++ internal/app/tokenised-infrastructure-rewarder/services/calculator.go
@@ -11,34 +11,6 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func sumMintedHashPowerForAllCollections(collections []types.Collection) float64 {
-	var totalMintedHashPowerForAllCollections float64
-
-	for _, collection := range collections {
-		for _, nft := range collection.Nfts {
-			if time.Now().Unix() > nft.DataJson.ExpirationDate {
-				log.Info().Msgf("Nft with denomId {%s} and tokenId {%s} and expirationDate {%d} has expired! Skipping....", collection.Denom.Id, nft.Id, nft.DataJson.ExpirationDate)
-				continue
-			}
-			totalMintedHashPowerForAllCollections += nft.DataJson.HashRateOwned
-		}
-	}
-
-	return totalMintedHashPowerForAllCollections
-}
-
-func calculatePercent(available float64, actual float64, reward btcutil.Amount) btcutil.Amount {
-	if available <= 0 || actual <= 0 || reward.ToBTC() <= 0 {
-		return btcutil.Amount(0)
-	}
-
-	payoutRewardPercent := float64(actual) / float64(available) * 100 // ex 100 from 1000 = 10%
-	calculatedReward := float64(reward) * payoutRewardPercent / 100   // ex 10% from 1000 = 100
-
-	// btcutil.Amount is int64 because satoshi is the lowest possible unit (1 satoshi = 0.00000001 bitcoin) and is an int64 in btc core code
-	return btcutil.Amount(calculatedReward)
-}
-
 // if the nft has been owned by two or more people you need to split this reward for each one of them based on the time of ownership
 // so a method that returns each nft owner for the time period with the time he owned it as percent
 // use this percent to calculate how much each one should get from the total reward
@@ -129,7 +101,72 @@ func (s *PayService) calculateNftOwnersForTimePeriodWithRewardPercent(ctx contex
 	return ownersWithPercentOwnedTime, nil
 }
 
+func (s *PayService) calculateHourlyMaintenanceFee(farm types.Farm, currentHashPowerForFarm float64) (btcutil.Amount, error) {
+	currentYear, currentMonth, _ := s.helper.Date()
+	periodLength := s.helper.DaysIn(currentMonth, currentYear)
+	mtFeeInBTC, err := strconv.ParseFloat(farm.MaintenanceFeeInBtc, 64)
+	if err != nil {
+		return -1, err
+	}
+	mtFeeInSatoshis, err := btcutil.NewAmount(mtFeeInBTC)
+	if err != nil {
+		return -1, err
+	}
+	feePerOneHashPower := btcutil.Amount(float64(mtFeeInSatoshis) / currentHashPowerForFarm)
+	dailyFeeInSatoshis := int(feePerOneHashPower) / periodLength
+	hourlyFeeInSatoshis := dailyFeeInSatoshis / 24
+	return btcutil.Amount(hourlyFeeInSatoshis), nil
+}
+
+func (s *PayService) calculateMaintenanceFeeForNFT(periodStart int64,
+	periodEnd int64,
+	hourlyFeeInSatoshis btcutil.Amount,
+	rewardForNft btcutil.Amount) (btcutil.Amount, btcutil.Amount, btcutil.Amount) {
+
+	periodInHoursToPayFor := (periodEnd - periodStart) / 3600                                              // period for which we are paying the MT fee
+	nftMaintenanceFeeForPayoutPeriod := btcutil.Amount(periodInHoursToPayFor * int64(hourlyFeeInSatoshis)) // the fee for the period
+	if nftMaintenanceFeeForPayoutPeriod > rewardForNft {                                                   // if the fee is greater - it has higher priority then the users reward
+		nftMaintenanceFeeForPayoutPeriod = rewardForNft
+		rewardForNft = 0
+	} else {
+		rewardForNft -= nftMaintenanceFeeForPayoutPeriod
+	}
+
+	partOfMaintenanceFeeForCudo := btcutil.Amount(float64(nftMaintenanceFeeForPayoutPeriod) * s.config.CUDOMaintenanceFeePercent / 100) // ex 10% from 1000 = 100
+	nftMaintenanceFeeForPayoutPeriod -= partOfMaintenanceFeeForCudo
+
+	return nftMaintenanceFeeForPayoutPeriod, partOfMaintenanceFeeForCudo, rewardForNft
+}
+
+func sumMintedHashPowerForAllCollections(collections []types.Collection) float64 {
+	var totalMintedHashPowerForAllCollections float64
+
+	for _, collection := range collections {
+		for _, nft := range collection.Nfts {
+			if time.Now().Unix() > nft.DataJson.ExpirationDate {
+				log.Info().Msgf("Nft with denomId {%s} and tokenId {%s} and expirationDate {%d} has expired! Skipping....", collection.Denom.Id, nft.Id, nft.DataJson.ExpirationDate)
+				continue
+			}
+			totalMintedHashPowerForAllCollections += nft.DataJson.HashRateOwned
+		}
+	}
+
+	return totalMintedHashPowerForAllCollections
+}
+
 func roundToPrecision(value float64) (result float64) {
 	result, _ = strconv.ParseFloat(fmt.Sprintf("%.2f", value), 64)
 	return
+}
+
+func calculatePercent(available float64, actual float64, reward btcutil.Amount) btcutil.Amount {
+	if available <= 0 || actual <= 0 || reward.ToBTC() <= 0 {
+		return btcutil.Amount(0)
+	}
+
+	payoutRewardPercent := float64(actual) / float64(available) * 100 // ex 100 from 1000 = 10%
+	calculatedReward := float64(reward) * payoutRewardPercent / 100   // ex 10% from 1000 = 100
+
+	// btcutil.Amount is int64 because satoshi is the lowest possible unit (1 satoshi = 0.00000001 bitcoin) and is an int64 in btc core code
+	return btcutil.Amount(calculatedReward)
 }
